476a30f1f86a5ed80e8bb85e8701bf6275668013
PierreDemailly <pierredemailly.pro@gmail.com>
2023-06-24 15:36:00 +0200

    chore: fix tests

--- test/walk.js
+++ test/walk.js
@@ -1,9 +1,8 @@
-import test from "node:test";
-import assert from "node:assert"
-
 // Require Node.js Dependencies
-import path from "path";
-import { fileURLToPath } from "url";
+import test from "node:test";
+import assert from "node:assert";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 // Require Internal Dependencies
 import { walk, walkSync } from "../index.js";
@@ -13,11 +12,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const kRootLocation = path.join(__dirname, "..");
 const kFixturesDir = path.join(__dirname, "fixtures");
-const kExpectedJSFiles = ["index.js", "test/walk.js"].map((fileLocation) =>
-  path.normalize(fileLocation)
-);
+const kExpectedJSFiles = ["index.js", "test/walk.js"].map((fileLocation) => path.normalize(fileLocation));
 
-test("should return all JavaScript files of the project (Asynchronously)", async (t) => {
+test("should return all JavaScript files of the project (Asynchronously)", async(t) => {
   const files = [];
   const options = { extensions: new Set([".js"]) };
 
@@ -30,31 +27,28 @@ test("should return all JavaScript files of the project (Asynchronously)", async
     }
   }
 
-  assert.deepEqual(files, kExpectedJSFiles);
+  assert.deepEqual(files.sort(), kExpectedJSFiles);
 });
 
-test("should return all JavaScript files of the project (Synchronously)", async (t) => {
+test("should return all JavaScript files of the project (Synchronously)", async(t) => {
   const options = { extensions: new Set([".js"]) };
 
   const files = [...walkSync(kRootLocation, options)]
     .filter(([dirent]) => dirent.isFile())
-    .map(([, absoluteFileLocation]) =>
-      path.relative(kRootLocation, absoluteFileLocation)
-    );
+    .map(([, absoluteFileLocation]) => path.relative(kRootLocation, absoluteFileLocation));
 
   assert.deepEqual(files, kExpectedJSFiles);
 });
 
-test("should return all files in the fixtures directory", async (t) => {
+test("should return all files in the fixtures directory", async(t) => {
   const files = [...walkSync(kFixturesDir)]
     .filter(([dirent]) => dirent.isFile())
-    .map(([, absoluteFileLocation]) =>
-      path.relative(kRootLocation, absoluteFileLocation)
-    );
+    .map(([, absoluteFileLocation]) => path.relative(kRootLocation, absoluteFileLocation));
 
   const expectedFiles = [
     "test/fixtures/foobar.txt",
-    "test/fixtures/test.md",
+    "test/fixtures/test.md"
   ].map((fileLocation) => path.normalize(fileLocation));
+
   assert.deepEqual(files, expectedFiles);
 });
